09417c9bfd752a95fafcb1543dfaf0c5a939ed58
kant003 <gonzalezm.angel@gmail.com>
2025-02-11 14:57:09 +0100

    offer detail

--- src/pages/OfferDetail.tsx
+++ src/pages/OfferDetail.tsx
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { OfferService } from "../services/offer.services"
+import Offer from "../models/Offer"
 
 function OfferDetail() {
+  const {id} = useParams()
+  const [offer, setOffer] = useState<Offer>()
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<string | null>(null)
+
+  useEffect(()=>{
+    setLoading(true)
+    OfferService
+      .getById(id)
+      .then(setOffer)
+      .catch(error => setError(error.message))
+      .finally(()=>setLoading(false))
+  },[id])
+
+  if(loading) return <div>Loading...</div>
+  if(error) return <div>Error: {error}</div>
+  if(!offer) return <div>Ofertas no encontradas</div>
+
   return (
-    <div>OfferDetail</div>
+    <div className="text-white">
+      <div>Titulo: {offer.title}</div>
+      <div>Descripcion: {offer.description}</div>
+      <div>Activo: {offer.active?'SI':'NO'}</div>
+      <div>Email de contacto: {offer.contactEmail}</div>
+      <div>Fecha publicación: {new Date(offer.published).toLocaleString()}</div>
+      <div>Fecha finalización: {new Date(offer.expired).toLocaleString()}</div>
+      <div>Localización:</div>
+      
+      <iframe width="100%" height="300" loading="lazy" 
+      src={`https://www.google.com/maps?q=${offer.location}&output=embed`}
+      >
+
+      </iframe>
+    </div>
   )
 }
 
