a2ccd3a02e29ce7fe98c8634d522ead27dd24a73
kant003 <gonzalezm.angel@gmail.com>
2025-02-13 09:44:28 +0100

    solve simple errors

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,8 +4,8 @@ function Navbar() {
   return (
     
 
-<nav class="bg-white border-gray-200 dark:bg-gray-900">
-<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+<nav className="bg-white border-gray-200 dark:bg-gray-900">
+<div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 
   <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
       <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo"/>
